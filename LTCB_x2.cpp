a032b787c407871d84b9fcda94b66334fef25b0b
thien5743 <thientran5743@gmail.com>
2024-03-11 14:12:43 +0700

    Add project files.

---
+++ LTCB_x2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cout << "nhap so vao day: ";
+	cin >> b;
+	 a = 1;
+	while (a <= 10) {
+		cout << a*b << endl;
+		a++;
+		}
+
+
+	
+		
+}
